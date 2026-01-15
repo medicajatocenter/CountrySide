306f258375c37f8ee2fb489929ac7ad685032db0
medicajatocenter <medicajatocenter4372@gmail.com>
2026-01-15 10:45:01 +0100

    Delete js/menuplusBibite.js

--- js/menuplusBibite.js
+++
@@ -1,7 +0,0 @@
-const MENU_PLUS_BIBITE = [
-  { id: "coca", nome: "Coca-Cola 33cl" },
-  { id: "coca_zero", nome: "Coca-Cola Zero 33cl" },
-  { id: "fanta", nome: "Fanta 33cl" },
-  { id: "acqua_nat", nome: "Acqua naturale" },
-  { id: "acqua_fri", nome: "Acqua frizzante" }
-];
